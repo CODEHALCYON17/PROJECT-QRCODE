a4db66caf1631f7463f886d338ebca61a6089aca
DATAPIRATE17 <124865981+DATAPIRATE17@users.noreply.github.com>
2023-09-01 18:47:36 +0530

    commit

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const input=document.getElementById('qr-text');
-const btn=document.getElementById('generate-btn');
-const divi=document.getElementsByClassName('qr-part');
+const input = document.getElementById('qr-text');
+const btn = document.getElementById('generate-btn');
+const divi = document.getElementsByClassName('qr-part');
 let isGenerated = false;
-var prevData=null;
-btn.addEventListener('click',()=>{
-    const data=input.value.trim();
-    let qrSrc="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
-    if(data.length>0){
-        if(prevData==null){
-            prevData=data;
+let prevData = null;
+divi[0].classList.add('hidden');
+
+btn.addEventListener('click', () => {
+    const data = input.value.trim();
+    let qrSrc = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+    if (data.length > 0) {
+        if (prevData == null) {
+            prevData = data;
         }
-        if(data==prevData && isGenerated){
+        if (data == prevData && isGenerated) {
             alert("Qr already generated for this text");
-        }
-        else{
-            divi[0].innerHTML='';
-            qrSrc+=data;
-            const qrimg=document.createElement('img');
-            qrimg.src=qrSrc;
-            qrimg.alt="QR Code";
+        } else {
+            divi[0].classList.remove('hidden');
+            divi[0].innerHTML = '';
+            qrSrc += data;
+            const qrimg = document.createElement('img');
+            qrimg.src = qrSrc;
+            qrimg.alt = "QR Code";
             divi[0].appendChild(qrimg);
-            const downBtn=document.createElement('button');
-            downBtn.textContent="Download";
-            downBtn.id="download-btn";
+            const downBtn = document.createElement('button');
+            downBtn.textContent = "Download";
+            downBtn.id = "download-btn";
             divi[0].appendChild(downBtn);
-            const dwnBtn = document.getElementById('download-btn');
-            dwnBtn.addEventListener('click', () => {
-            const link = document.createElement('a');
-            link.href = qrimg.src;
-            downloadName=data;
-            link.download = `${data}qr.png`;
-                fetch(qrimg.src)
-            .then(response => response.blob())
-            .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            link.href = url;
-            link.click();
-            window.URL.revokeObjectURL(url);
-            });
-            });
-            previousData = data; 
+
+            // Gradually increase opacity for the elements
+            setTimeout(() => {
+                qrimg.style.opacity = 1;
+                btn.style.opacity = 1;
+                downBtn.style.opacity = 1;
+            }, 100);
+
+            previousData = data;
             isGenerated = true;
         }
-    }
-    else
-    {
+    } else {
         alert("please enter some text to generate");
+    }
+});
+
+input.addEventListener('keyup', () => {
+    if (input.value.length === 0) {
+        // Add the 'hidden' class to hide .qr-part when text is cleared
+        divi[0].classList.add('hidden');
+        divi[0].innerHTML = '';
+        isGenerated = false;
     }
 });
